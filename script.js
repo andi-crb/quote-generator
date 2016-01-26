12b44557db191aba4f2a7552e6f53769725294ad
Andi <andicbuchanan@gmail.com>
2016-01-27 11:29:17 +1300

    got button and quotes on page working

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 var quotes = ["In the midst of winter, I found there was, within me, an invincible summer. - Albert Camus", "When one burns one's bridges, what a very nice fire it makes. - Dylan Thomas", "I read the newspapers avidly, it is my one continuous source of fiction. - Nye Bevan", "Regard all art critics as useless and dangerous. - Manifesto of the Futurists", "I talk to God but the sky is empty - Sylvia Plath", "The only interesting answers are those which destroy the question - Susan Sontag"];
+function getQuote () {
   var quoteReturned = quotes[Math.floor(Math.random()*quotes.length)];
-alert(quoteReturned)
+  console.log(quoteReturned);
+  document.getElementById("quote").innerHTML = quoteReturned;
+}
